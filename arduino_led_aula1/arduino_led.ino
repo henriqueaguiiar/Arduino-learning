b38be4b9fc5d62bc1399f2f9b74ecefe48c4a1c1
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2025-02-20 14:24:53 -0300

    Created circuit and adding led's

---
+++ arduino_led_aula1/arduino_led.ino
@@ -0,0 +1,12 @@
+// C++ code
+// Projeto Aula 1 Arduino no tinkercad.
+void setup(){
+  pinMode(2, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(2, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(2, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+}
